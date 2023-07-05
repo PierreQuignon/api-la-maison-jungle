d28df14f3c7a72d899fb4bf63e5ab1526a277094
Pierre Quignon <p.quignon.pro@gmail.com>
2023-07-05 15:40:52 +0200

    made the restfull API

--- database.sql
+++ database.sql
@@ -3,4 +3,5 @@ CREATE DATABASE la_maison_jungle;
 CREATE TABLE plant(
   plant_id SERIAL PRIMARY KEY,
   name VARCHAR(100)
+  water INTEGER CHECK (water >= 1 AND water <= 500)
 );
--- index.js
+++ index.js
@@ -12,10 +12,11 @@ app.use(express.json());
 //create plant
 app.post("/plants", async (req, res) => {
   try {
-    const { name } = req.body;
-    const newPlant = await pool.query("INSERT INTO plant (name) VALUES($1) RETURNING *", [
-      name,
-    ]);
+    const { name, water } = req.body;
+    const newPlant = await pool.query(
+      "INSERT INTO plant (name, water) VALUES($1, $2) RETURNING *",
+      [name, water]
+    );
     res.json(newPlant.rows[0]);
   } catch (error) {
     console.error(error.message);
@@ -33,10 +34,47 @@ app.get("/plants", async (req, res) => {
 });
 
 //get a plant
+app.get("/plants/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const plant = await pool.query(
+      "SELECT * FROM plant WHERE plant_id = $1",
+      [id]
+    );
+    res.json(plant.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 // update a todo
+app.put("/plants/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, water } = req.body;
+    const updtatePlant = await pool.query(
+      "UPDATE plant SET name = $1, water = $2 WHERE plant_id = $3",
+      [name, water, id]
+    );
+    res.json("the plant was updated with succes !");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 // delete a todo
+app.delete("/plants/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletePlant = await pool.query(
+      "DELETE FROM plant WHERE plant_id = $1",
+      [id]
+    );
+    res.json("the plant was deleted with succes !");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("the server has started");
